9b194c58179e7ca44bffb71522be083e585eed3a
Shaikh6767 <85774746+mumair738@users.noreply.github.com>
2026-01-11 17:09:13 +0530

    Update minikit.config.ts

--- minikit.config.ts
+++ minikit.config.ts
@@ -3,7 +3,7 @@ const ROOT_URL =
   (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000');
 
 /**
- * MiniApp configuration object. Must follow the Farcaster MiniApp specification.
+ * MiniApp configuration object. Must follow the Farcaster Mini specification.
  *
  * @see {@link https://miniapps.farcaster.xyz/docs/guides/publishing}
  */
